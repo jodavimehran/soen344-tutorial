f4b6a3f5964d3d95f2e313faadbdf89ae5f7046a
Mehran Jodavi <m_jodavi@encs.concordia.ca>
2020-01-23 13:14:41 -0400

    #1: A method to generate a sample TreeNode is added

--- src/main/java/ca/concordia/soen344/composite/CompositePatternClient.java
+++ src/main/java/ca/concordia/soen344/composite/CompositePatternClient.java
@@ -1,6 +1,7 @@
 package ca.concordia.soen344.composite;
 
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
 import java.io.File;
 
 public class CompositePatternClient {
@@ -19,7 +20,24 @@ public class CompositePatternClient {
             System.out.println(topDir.ls());
             System.out.println(topDir.size());
             System.out.println(topDir.countFiles());
-
+            
         }
+    }
+
+    /**
+     * This method provides a sample TreeNode
+     *
+     * @return a sample TreeNode which contains five parent. Each single parent is contains some childes that is vary from one to five.
+     */
+    private static DefaultMutableTreeNode createSampleTreeNode() {
+        DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("Root");
+        for (int i = 1; i <= 5; i++) {
+            DefaultMutableTreeNode parentNode = new DefaultMutableTreeNode("Parent" + i);
+            rootNode.add(parentNode);
+            for (int j = 1; j <= i; j++) {
+                parentNode.add(new DefaultMutableTreeNode("Child" + i + j));
+            }
+        }
+        return rootNode;
     }
 }
